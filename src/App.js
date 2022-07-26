0e3315c6a57913222f3a7461bc2bd7d27b6a4480
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-07-26 22:42:14 +0600

    🏷️🛠️📕💅components added and completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import Benefit from "./Components/Benefit";
+import Footer from "./Components/Footer";
+import Home from "./Components/Home";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Home />
+      <Benefit />
+      <Footer />
+    </Container>
   );
 }
 
-export default App;
+const Container = styled.main`
+  max-width: 1440px;
+  margin: 0 auto;
+`;
